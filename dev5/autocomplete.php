117a0fc70c89033a98a6f7de20c42bdd524c8131
Loja Dev5™ <49690959+lojadev5@users.noreply.github.com>
2022-03-09 22:28:30 -0300

    Update 1.1

---
+++ dev5/autocomplete.php
@@ -0,0 +1,31 @@
+<?php
+namespace __Dev5\BasicOpenCart;
+
+/**
+ * @package   Basic OpenCart AutoComplete for PHPStorm
+ * @version   1.1
+ * @author    Vitor Reis - Loja Dev5™
+ * @link      https://dev5.com.br
+ *
+ * @property \Config $config
+ * @property \Log log
+ * @property \Event event
+ * @property \Loader load
+ * @property \Request request
+ * @property \Response response
+ * @property \Db db
+ * @property \Session session
+ * @property \Cache cache
+ * @property \Url url
+ * @property \Language language
+ * @property \Document document
+ * @property \Cart\Customer customer
+ * @property \Cart\Currency currency
+ * @property \Cart\Tax tax
+ * @property \Cart\Weight weight
+ * @property \Cart\Length length
+ * @property \Cart\Cart cart
+ * @property \Encryption encryption
+ * @property \Cart\User user;
+ */
+abstract class Autocomplete {}
